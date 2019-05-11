69fda6e1fccfcd853020e0b5dcc9c5f2d99810b3
Manoa <manoa9438@gmail.com>
2019-05-11 12:06:56 +0200

    making so much good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+
+}
