bfd1a22dfc48c774d3949d5c56e452dfedea8178
Manoa <manoa9438@gmail.com>
2019-05-14 11:23:46 +0200

    quete 5 symphony

---
+++ src/Entity/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $string;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    public function setString(?string $string): self
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+}
